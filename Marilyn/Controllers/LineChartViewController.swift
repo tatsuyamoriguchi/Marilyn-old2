4fc2216cfb1ef51cecb41061030fdcc8276ae862
tatsuyamoriguchi <caliosdev@gmail.com>
2019-05-15 02:42:43 -0700

    LineChartVC Core Data predicate with location and time period

--- Marilyn/Controllers/LineChartViewController.swift
+++ Marilyn/Controllers/LineChartViewController.swift
@@ -12,23 +12,51 @@ import CoreData
 class LineChartViewController: UIViewController {
 
     
-    private let dataSource = ["All", "Home", "Work", "School", "Grocery Store", "Ramen Shop", "Cafe", "On the Road"]
+    //private let dataSource = ["All", "Home", "Work", "School", "Grocery Store", "Ramen Shop", "Cafe", "On the Road"]
     private var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>?
     var locationArray: [Any] = []
+    var somArray: [Date] = []
+    var locaName: String? = "All"
     
     @IBOutlet weak var LocationPicker: UIPickerView!
     
+    
     @IBAction func hrsOnPressed(_ sender: UIButton) {
+        calculateRate(timeRangeString: "24hrs")
+        mockupDisplay()
     }
     @IBAction func daysOnPressed(_ sender: UIButton) {
+        calculateRate(timeRangeString: "7days")
+        mockupDisplay()
     }
     @IBAction func monthOnPressed(_ sender: UIButton) {
+        calculateRate(timeRangeString: "1month")
+        mockupDisplay()
     }
     @IBAction func yearOnPressed(_ sender: UIButton) {
+        calculateRate(timeRangeString: "1year")
+        mockupDisplay()
     }
     @IBAction func allTimeOnPressed(_ sender: UIButton) {
+        calculateRate(timeRangeString: "all")
+        mockupDisplay()
     }
     
+    func mockupDisplay() {
+       /* var somArrayString: String = ""
+        for i in somArray {
+            print("i: \(i)")
+           // let today = Date()
+            //today.toString(dateFormat: "MM-dd-yyyy-hh-mm-ss")
+            somArrayString = i.toString(style: .long)
+            print("somArrayString: \(somArrayString)")
+            somArrayString.append(somArrayString)
+        }
+        */
+        textView.text = "somArray: \(somArray)"
+        print("final somArray: \(somArray)")
+    }
+
     //// Mock up for graphic line chart, for now testing purpose,
     // just show row values
     @IBOutlet weak var textView: UITextView!
@@ -39,24 +67,27 @@ class LineChartViewController: UIViewController {
         LocationPicker.dataSource = self
         LocationPicker.delegate = self
 
-        configureFetchedResultsController(EntityName: "Location")
-        populatepickerView()
-
+        configureFetchedResultsController(EntityName: "Location", sortString: "timeStamp")
+        
+        populateLocationData()
         
     }
     
+    // To reload UIPickerView data with new location data
     override func viewDidAppear(_ animated: Bool) {
+        // To avoid append array data to itself
+        locationArray = []
         viewDidLoad()
     }
 
-    func configureFetchedResultsController(EntityName: String) {
+    func configureFetchedResultsController(EntityName: String, sortString: String) {
         
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         
         // Create the fetch request, set some sort descriptor, then feed the fetchedResultsController
         // the request with along with the managed object context, which we'll use the view context
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: EntityName)
-        let sortDescriptorType = NSSortDescriptor(key: "timeStamp", ascending: false)
+        let sortDescriptorType = NSSortDescriptor(key: sortString, ascending: false)
         
         fetchRequest.sortDescriptors = [sortDescriptorType]
         fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: appDelegate.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
@@ -69,44 +100,138 @@ class LineChartViewController: UIViewController {
         
     }
     
-    
+    func calculateRate(timeRangeString: String) {
+        
+        configureFetchedResultsController(EntityName: "StateOfMind", sortString: "timeStamp")
+        
+        switch timeRangeString {
+        case "24hrs":
+            print("24hrs")
+            let endDate = Date()
+            //let startDate = Calendar.current.date(byAdding: .day, value: -1, to: endDate)
+            let startDate = Calendar.current.date(byAdding: .hour, value: -7, to: endDate)
+            //print("startDate: \(startDate)")
+            //print("endDate: \(endDate)")
+            populateSOMData(startDate: startDate!, endDate: endDate, selectedLocationName: locaName!)
+            
+        case "7days":
+            print("7days")
+            let endDate = Date()
+            let startDate = Calendar.current.date(byAdding: .day, value: -7, to: endDate)
+            
+            populateSOMData(startDate: startDate!, endDate: endDate, selectedLocationName: locaName!)
 
-    /*
-    // MARK: - Navigation
+        case "1month":
+            print("1month")
+            let endDate = Date()
+            let startDate = Calendar.current.date(byAdding: .month, value: -1, to: endDate)
+            
+            populateSOMData(startDate: startDate!, endDate: endDate, selectedLocationName: locaName!)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        case "1year":
+            print("1year")
+            let endDate = Date()
+            let startDate = Calendar.current.date(byAdding: .year, value: -1, to: endDate)
+            
+            populateSOMData(startDate: startDate!, endDate: endDate, selectedLocationName: locaName!)
+
+        default:
+            print("default all")
+            let endDate = Date()
+            let startDate = Calendar.current.date(byAdding: .year, value: 0, to: endDate)
+            populateSOMData(startDate: startDate!, endDate: endDate, selectedLocationName: locaName!)
+
+        }
     }
-    */
 
-    func populatepickerView(){
+
+    func populateSOMData(startDate: Date, endDate: Date, selectedLocationName: String){
+        somArray = []
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let managedContext = appDelegate?.persistentContainer.viewContext
+        var items = [StateOfMind]()
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "StateOfMind")
+        
+        print("selectedLocationName: \(selectedLocationName)")
+        
+/*        if selectedLocationName != "All" {
+            fetchRequest.predicate = NSPredicate(format: "location.locationName == %@", selectedLocationName)
+            print("Howdy!")
+            
+        }
+  */
+        // selectedLocaitonName = "All" && startDate == endDate
+/*
+        if selectedLocationName != "All" {
+            if startDate != endDate {
+                //fetchRequest.predicate = NSPredicate(format: "(timeStamp >= %@) AND (timeStamp < %@)", startDate as CVarArg, endDate as CVarArg)
+                fetchRequest.predicate = NSPredicate(format: "(location.locationName == %@) AND (timeStamp >= %@) AND (timeStamp < %@)", selectedLocationName, startDate as CVarArg, endDate as CVarArg)
+            }
+            else {
+                fetchRequest.predicate = NSPredicate(format: "location.locationName == %@", selectedLocationName)
+            }
+        } else if startDate != endDate {
+            fetchRequest.predicate = NSPredicate(format: "(timeStamp >= %@) AND (timeStamp < %@)", startDate as CVarArg, endDate as CVarArg)
+        } else {}
+*/
+        
+        // selectedLocationName : startDate == endDate
+        // All : All Time(true)
+        // All : limited time
+        // Location : All time
+        // Locaiton : Limited time
+        
+        if (selectedLocationName != "All") && (startDate != endDate) {
+            //fetchRequest.predicate = NSPredicate(format: "(timeStamp >= %@) AND (timeStamp < %@)", startDate as CVarArg, endDate as CVarArg)
+            fetchRequest.predicate = NSPredicate(format: "(location.locationName == %@) AND (timeStamp >= %@) AND (timeStamp < %@)", selectedLocationName, startDate as CVarArg, endDate as CVarArg)
+            
+        } else if (selectedLocationName != "All") && (startDate == endDate) {
+            fetchRequest.predicate = NSPredicate(format: "location.locationName == %@", selectedLocationName)
+            
+        } else if (selectedLocationName == "All") && (startDate != endDate) {
+            fetchRequest.predicate = NSPredicate(format: "(timeStamp >= %@) AND (timeStamp < %@)", startDate as CVarArg, endDate as CVarArg)
+            
+        } else {}
+
+    
+    
+        
+        let sortDescriptorTypeTime = NSSortDescriptor(key: "timeStamp", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptorTypeTime]
+        
+        do { items = try managedContext?.fetch(fetchRequest) as! [StateOfMind]
+            for item in items {
+                somArray.append(item.timeStamp!)
+                print("item.location.locationName: \(item.location?.locationName)")
+            }
+        } catch {
+            print("Error")
+        }
+    }
+    
+    func populateLocationData(){
         //var resultData: [String]
         let appDelegate = UIApplication.shared.delegate as? AppDelegate
         let managedContext = appDelegate?.persistentContainer.viewContext
-        var locations = [Location]()
+        var items = [Location]()
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Location")
         
         let sortDescriptorTypeTime = NSSortDescriptor(key: "locationName", ascending: true)
         fetchRequest.sortDescriptors = [sortDescriptorTypeTime]
         
-        do { locations = try managedContext?.fetch(fetchRequest) as! [Location]
-            
-            for item in locations {
+        do { items = try managedContext?.fetch(fetchRequest) as! [Location]
+            for item in items {
                 locationArray.append(item.locationName as Any)
-                
             }
-            //print(locationArray)
-            
         } catch {
             print("Error")
         }
-        
+        locationArray.insert("All", at: 0)
+        print("locationArray: \(locationArray)")
     }
 
-    
 }
+
 
 extension LineChartViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -120,8 +245,8 @@ extension LineChartViewController: UIPickerViewDelegate, UIPickerViewDataSource 
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
-        //textView.text = dataSource[row]
-        textView.text = locationArray[row] as? String
+        //textView.text = locationArray[row] as? String
+        locaName = locationArray[row] as? String ?? "All"
         
     }
     
@@ -129,7 +254,12 @@ extension LineChartViewController: UIPickerViewDelegate, UIPickerViewDataSource 
         //return dataSource[row]
         return locationArray[row] as? String
     }
-    
-    
-    
+}
+
+extension Date {
+    func toString(style : DateFormatter.Style) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = style
+        return dateFormatter.string(from: self)
+    }
 }
